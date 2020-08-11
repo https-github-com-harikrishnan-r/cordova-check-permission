17ac0b9d4da9f8906080756975e11dba8687bee3
harikrir <hkrishnan254@gmail.com>
2020-08-12 00:16:47 +0530

    Update CheckGPS.java

--- src/android/CheckGPS.java
+++ src/android/CheckGPS.java
@@ -7,6 +7,22 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
+//import android.support.v4.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
 import android.location.LocationManager;
 import android.content.Context;
 /*
@@ -16,13 +32,17 @@ public class CheckGPS extends CordovaPlugin{
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		if (action.equals("check")){
-			this.check(callbackContext);
+			this.requestContactPermission(callbackContext);
 			return true;
 		}
 		return false;
 	}
 
 	private void check(CallbackContext callbackContext){
+		
+		
+		
+		
 		Context context = this.cordova.getActivity().getApplicationContext();
     final LocationManager manager = (LocationManager) context.getSystemService( Context.LOCATION_SERVICE );
     if ( manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
@@ -31,4 +51,66 @@ public class CheckGPS extends CordovaPlugin{
 			callbackContext.error(0);
 		}
   }
+  
+  
+  public void requestContactPermission(CallbackContext CallbackContext) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        android.Manifest.permission.READ_CONTACTS)) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setTitle("Read Contacts permission");
+                    builder.setPositiveButton(android.R.string.ok, null);
+                    builder.setMessage("Please enable access to contacts.");
+                    builder.setOnDismissListener(new DialogInterface.OnDismissListener() {
+                        @TargetApi(Build.VERSION_CODES.M)
+                        @Override
+                        public void onDismiss(DialogInterface dialog) {
+                            requestPermissions(
+                                    new String[]
+                                            {android.Manifest.permission.READ_CONTACTS}
+                                    , PERMISSIONS_REQUEST_READ_CONTACTS);
+                        }
+                    });
+                    builder.show();
+                } else {
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{android.Manifest.permission.READ_CONTACTS},
+                            PERMISSIONS_REQUEST_READ_CONTACTS);
+                }
+            } else {
+				callbackContext.success();
+               return true;
+            }
+        } else {
+			callbackContext.success();
+           return true;
+        }
+    }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_READ_CONTACTS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                return true;
+                } else {
+                    Toast.makeText(this, "You have disabled a contacts permission", Toast.LENGTH_LONG).show();
+					return false;
+                }
+                return;
+            }
+        }
+  
+}
 }
